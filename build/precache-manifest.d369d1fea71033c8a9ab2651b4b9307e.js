8974bef8a4b7787d599814eafbb02dd9e3bb35e0
KomonWorker <1126972456@qq.com>
2019-10-28 18:09:07 +0800

    测试

---
+++ build/precache-manifest.d369d1fea71033c8a9ab2651b4b9307e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efdfd200ec14984fe0b119a0a32c67bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24c496c770d49f49a9a7",
+    "url": "/static/css/main.20688d58.chunk.css"
+  },
+  {
+    "revision": "46ce34e96f26178eb814",
+    "url": "/static/js/2.c63eec68.chunk.js"
+  },
+  {
+    "revision": "24c496c770d49f49a9a7",
+    "url": "/static/js/main.230ff219.chunk.js"
+  },
+  {
+    "revision": "e5a23b73db251966e114",
+    "url": "/static/js/runtime-main.e512ea7c.js"
+  },
+  {
+    "revision": "dc7ad8ec3de8b6be2075434de5c3e2cd",
+    "url": "/static/media/logo.dc7ad8ec.png"
+  }
+]);
