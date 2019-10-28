35f4a8c08a022c1d6827b75dfbdf0ef27cc83b15
KomonWorker <1126972456@qq.com>
2019-10-28 19:08:47 +0800

    测试页面

---
+++ precache-manifest.af393452c0d53d2880853c5cc27d4dc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823504c42ca943158e9722711444633b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "69b4ea969f2cd7ebb8e3",
+    "url": "./static/css/2.e0b1c963.chunk.css"
+  },
+  {
+    "revision": "e9aafc9a111732108acc",
+    "url": "./static/css/main.20688d58.chunk.css"
+  },
+  {
+    "revision": "69b4ea969f2cd7ebb8e3",
+    "url": "./static/js/2.9f0a5d50.chunk.js"
+  },
+  {
+    "revision": "e9aafc9a111732108acc",
+    "url": "./static/js/main.457f1a3a.chunk.js"
+  },
+  {
+    "revision": "3ea30b38655962880dfd",
+    "url": "./static/js/runtime-main.f4ecdf2a.js"
+  },
+  {
+    "revision": "dc7ad8ec3de8b6be2075434de5c3e2cd",
+    "url": "./static/media/logo.dc7ad8ec.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dbd2b7bd992fbbf8ddde52d3ce7c7bb8.js"
+  "./precache-manifest.af393452c0d53d2880853c5cc27d4dc4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
