5a6d47d6acec1dfdd3faceb98b6569dc0237eb33
KomonWorker <1126972456@qq.com>
2019-10-28 19:02:33 +0800

    页面测试

--- src/utils/filterDate.js
+++
@@ -1,7 +0,0 @@
-/*
-    时间格式化
-*/
-export const filterDate = date => {
-  return `${date.getFullYear()}年${date.getMonth() +
-    1}月${date.getDate()}日${date.getHours()}时${date.getMinutes()}分${date.getSeconds()}秒`;
-};
